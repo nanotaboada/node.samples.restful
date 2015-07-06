91d2246bc6c00327022f0ddc19c883778230a4bb
Nano Taboada <nanotaboada@msn.com>
2015-07-06 16:01:14 -0300

    Updated unit tests to SuperTest syntax.

--- test/books.js
+++ test/books.js
@@ -2,83 +2,73 @@
 var should = require('should'),
     assert = require('assert'),
 	request = require('supertest'),
-	url = 'http://node-samples-restful.herokuapp.com/',
-	valid = '9781449331818',
-	invalid = 'foobarbaz';
-	
+
+request = request('http://node-samples-restful.herokuapp.com');
+
+var valid = '9781449331818',
+	invalid = 'foobarbaz';	
+
 describe('Books', function() {
 	describe('/books', function() {
 		it('should be HTTP 401 when HTTP GET to all resources is not authenticated.', function(done) {
-			request(url)
+			request
 			.get('/api/v1/books')
-			.end(function (error, response){
-				if (error) {
-				    throw error;
-				};
-				response.should.have.status(401);
-			});
-		    done();
+			.expect(401)
+			.end(function(error, response){
+        		if (error) return done(error);
+        		done()
+      		});
 		});
 	});
 	describe('/book', function() {
 		it('should be HTTP 401 when HTTP GET to valid resource is not authenticated.', function(done) {
-			request(url)
+			request
 			.get('/api/v1/book/' + valid)
-			.end(function (error, response){
-				if (error) {
-				    throw error;
-				};
-				response.should.have.status(401);
-			});
-		    done();
+			.expect(401)
+			.end(function(error, response){
+        		if (error) return done(error);
+        		done()
+      		});
 		});
 		it('should be HTTP 401 when HTTP GET to invalid resource is not authenticated.', function(done) {
-			request(url)
+			request
 			.get('/api/v1/book/' + invalid)
-			.end(function (error, response){
-				if (error) {
-				    throw error;
-				};
-				response.should.have.status(401);
-			});
-		    done();
+			.expect(401)
+			.end(function(error, response){
+        		if (error) return done(error);
+        		done()
+      		});
 		});
 		it('should be HTTP 401 when HTTP POST is not authenticated.', function(done) {
 			var body = { isbn : valid };
-			request(url)
-			.post('/api/v1/book/')
+			request
+			.post('/api/v1/book')
 			.send(body)
-			.end(function (error, response){
-				if (error) {
-				    throw error;
-				};
-				response.should.have.status(401);
-			});
-		    done();
+			.expect(401)
+			.end(function(error, response){
+        		if (error) return done(error);
+        		done()
+      		});
 		});
 		it('should be HTTP 401 when HTTP PUT is not authenticated.', function(done) {
 			var body = { isbn : valid };
-			request(url)
-			.put('/api/v1/book/' + valid)
+			request
+			.put('/api/v1/book' + valid)
 			.send(body)
-			.end(function (error, response){
-				if (error) {
-				    throw error;
-				};
-				response.should.have.status(401);
-			});
-		    done();
+			.expect(401)
+			.end(function(error, response){
+        		if (error) return done(error);
+        		done()
+      		});
 		});
 		it('should be HTTP 401 when HTTP DELETE is not authenticated.', function(done) {
-			request(url)
-			.delete('/api/v1/book/' + valid)
-			.end(function (error, response){
-				if (error) {
-				    throw error;
-				};
-				response.should.have.status(401);
-			});
-		    done();
+			request
+			.delete('/api/v1/book' + valid)
+			.expect(401)
+			.end(function(error, response){
+        		if (error) return done(error);
+        		done()
+      		});
 		});
 	});
 });
--- test/users.js
+++ test/users.js
@@ -2,83 +2,73 @@
 var should = require('should'),
     assert = require('assert'),
 	request = require('supertest'),
-	url = 'http://node-samples-restful.herokuapp.com/',
-	valid = 1,
+
+request = request('http://node-samples-restful.herokuapp.com');
+
+var valid = 1,
 	invalid = 'foobarbaz';
-	
+
 describe('Users', function() {
 	describe('/users', function() {
 		it('should be HTTP 401 when HTTP GET to all resources is not authenticated.', function(done) {
-			request(url)
+			request
 			.get('/api/v1/users')
-			.end(function (error, response){
-				if (error) {
-				    throw error;
-				};
-				response.should.have.status(401);
-			});
-		    done();
+			.expect(401)
+			.end(function(error, response){
+        		if (error) return done(error);
+        		done()
+      		});
 		});
 	});
 	describe('/user', function() {
 		it('should be HTTP 401 when HTTP GET to valid resource is not authenticated.', function(done) {
-			request(url)
+			request
 			.get('/api/v1/user/' + valid)
-			.end(function (error, response){
-				if (error) {
-				    throw error;
-				};
-				response.should.have.status(401);
-			});
-		    done();
+			.expect(401)
+			.end(function(error, response){
+        		if (error) return done(error);
+        		done()
+      		});
 		});
 		it('should be HTTP 401 when HTTP GET to invalid resource is not authenticated.', function(done) {
-			request(url)
+			request
 			.get('/api/v1/user/' + invalid)
-			.end(function (error, response){
-				if (error) {
-				    throw error;
-				};
-				response.should.have.status(401);
-			});
-		    done();
+			.expect(401)
+			.end(function(error, response){
+        		if (error) return done(error);
+        		done()
+      		});
 		});
 		it('should be HTTP 401 when HTTP POST is not authenticated.', function(done) {
 			var body = { isbn : valid };
-			request(url)
-			.post('/api/v1/user/')
+			request
+			.post('/api/v1/user')
 			.send(body)
-			.end(function (error, response){
-				if (error) {
-				    throw error;
-				};
-				response.should.have.status(401);
-			});
-		    done();
+			.expect(401)
+			.end(function(error, response){
+        		if (error) return done(error);
+        		done()
+      		});
 		});
 		it('should be HTTP 401 when HTTP PUT is not authenticated.', function(done) {
 			var body = { isbn : valid };
-			request(url)
-			.put('/api/v1/user/' + valid)
+			request
+			.put('/api/v1/user' + valid)
 			.send(body)
-			.end(function (error, response){
-				if (error) {
-				    throw error;
-				};
-				response.should.have.status(401);
-			});
-		    done();
+			.expect(401)
+			.end(function(error, response){
+        		if (error) return done(error);
+        		done()
+      		});
 		});
 		it('should be HTTP 401 when HTTP DELETE is not authenticated.', function(done) {
-			request(url)
-			.delete('/api/v1/user/' + valid)
-			.end(function (error, response){
-				if (error) {
-				    throw error;
-				};
-				response.should.have.status(401);
-			});
-		    done();
+			request
+			.delete('/api/v1/user' + valid)
+			.expect(401)
+			.end(function(error, response){
+        		if (error) return done(error);
+        		done()
+      		});
 		});
 	});
 });
