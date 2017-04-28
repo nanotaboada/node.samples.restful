6afa3600df024e07721e8d712fe0f1e099e6766e
Nano Taboada <nanotaboada@msn.com>
2017-04-28 15:44:41 -0300

    Simplified Status responses.

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,30 +1,31 @@
 /*jslint unparam: true, node: true */
 
-// TODO: Dependency Injection
-// https://blog.risingstack.com/dependency-injection-in-node-js/
 var bookModel = require("../models/bookModel.js");
+
+// INFO: Using HTTP Methods for RESTful Services
+// http://www.restapitutorial.com/lessons/httpmethods.html
 
 var bookController = {
     /** Creates a new Book on the Model. */
-    post: function (request, response) {
+    post: function(request, response) {
         "use strict";
         var book = request.body;
         if (book.isbn) {
             var existing = bookModel.read(book.isbn);
             if (existing) {
-                response.status(409).json({ status: 409, message: "Conflict" });
+                response.status(409).end();
             } else {
                 bookModel.create(book);
                 response.location("/books/" + book.isbn);
-                response.status(201).json({ status: 201, message: "Created" });
+                response.status(201).end();
             }
         } else {
-            response.status(400).json({ status: 400, message: "Bad Request" });
+            response.status(400).end();
         }
     },
 
     /** Retrieves a Book by Id from the Model. */
-    get: function (request, response) {
+    get: function(request, response) {
         "use strict";
         // ISBN guaranteed by Router
         var isbn = request.params.id;
@@ -32,12 +33,12 @@ var bookController = {
         if (book) {
             response.status(200).json(book);
         } else {
-            response.status(404).json({ status: 404, message: "Not Found" });
+            response.status(404).end();
         }
     },
 
     /** Retrieves all Books on the Model. */
-    getAll: function (request, response) {
+    getAll: function(request, response) {
         "use strict";
         var books = bookModel.readAll();
         // Collection guaranteed by books.sqlite3 -- no need for 204 "No Content"
@@ -45,7 +46,7 @@ var bookController = {
     },
 
     /** Updates a Book on the Model. */
-    put: function (request, response) {
+    put: function(request, response) {
         "use strict";
         // ISBN guaranteed by Router
         var isbn = request.params.id;
@@ -54,22 +55,22 @@ var bookController = {
             var existing = bookModel.read(isbn);
             if (existing) {
                 bookModel.update(updated, existing);
-                response.status(204).json({ status: 204, message: "No Content" });
+                response.status(204).end();
             } else {
-                response.status(404).json({ status: 404, message: "Not Found" });
+                response.status(404).end();
             }
         } else {
-            response.status(400).json({ status: 400, message: "Bad Request" });
+            response.status(400).end();
         }
     },
 
     /** Deletes a Book by ISBN on the Model. */
-    delete: function (request, response) {
+    delete: function(request, response) {
         "use strict";
         // ISBN guaranteed by Router
         var isbn = request.params.id;
         bookModel.delete(isbn);
-        response.status(204).json({ status: 204, message: "No Content" });
+        response.status(204).end();
     }
 
 };
